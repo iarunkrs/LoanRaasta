903af0895394de043ec0fd3330cdfc7dd4789c75
iarunkrs <iarunkrs@gmail.com>
2024-10-20 18:37:35 +0530

    Delete pages directory

--- pages/Loan/BusinessLoan/BusinessLoan.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BusinessLoan = () => {
-  return (
-    <div>BusinessLoan</div>
-  )
-}
-
-export default BusinessLoan
--- pages/Loan/ProfessionalLoan/ProfessionalLoan.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProfessionalLoan = () => {
-  return (
-    <div>ProfessinalLoan</div>
-  )
-}
-
-export default ProfessionalLoan
